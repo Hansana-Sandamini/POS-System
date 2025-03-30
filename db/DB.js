5b0a6271962fee47915726cec8c3375784ebb284
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-03-30 23:33:12 +0530

    Added DB.js file inside of db folder

---
+++ db/DB.js
@@ -0,0 +1,40 @@
+export let Customers = [
+    {
+    custId: "C00-001",
+    custName: "Sadun Malaka",
+    custAddress: "No.17, Panadura",
+    custSalary: "100",
+    },
+
+    {
+    custId: "C00-002",
+    custName: "Ramal Wasuka",
+    custAddress: "Panadura",
+    custSalary: "200",
+    },
+];
+
+export let Items = [
+    {
+    itemId: "I00-001",
+    itemName: "Lux Bundle",
+    itemQty: 100,
+    itemPrice: 48,
+    },
+    {
+    itemId: "I00-002",
+    itemName: "Salmon",
+    itemQty: 300,
+    itemPrice: 150,
+    },
+    {
+    itemId: "I00-003",
+    itemName: "Anchor",
+    itemQty: 100,
+    itemPrice: 340,
+    },
+];
+
+export let Orders = [];
+  
+  
