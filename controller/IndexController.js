03674ead8a2690877389d513b00dba09d4f08ee1
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-03-31 00:35:23 +0530

    Added IndexController.js file

---
+++ controller/IndexController.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+$("#homePage").show().css('display', 'flex');
+$(".sectionName").text("POS System");
+$("header").css("background-color", "#3498db");
+
+$(".nav-link").click(function (event) {
+    event.preventDefault();
+
+    $("section").hide();
+
+    var targetSection = $(this).attr("href");
+
+    $(targetSection).show();
+    switch (targetSection) {
+    case "#CustomerManage":
+        $(".sectionName").text("Customer Manage");
+        break;
+    case "#ItemManage":
+        $(".sectionName").text("Item Manage");
+        break;
+    case "#OrderManage":
+        $(".sectionName").text("Order Manage");
+        break;
+    default:
+        $(".sectionName").text("POS System");
+    //   $("header").css("background-color", "#3498db");
+    }
+});
+});
